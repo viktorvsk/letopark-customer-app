f065b08e11df01cb808d1ac430089b2a55141ae6
viktorvsk <me@viktorvsk.com>
2020-05-27 12:42:23 +0300

    Better Orders progress

--- src/Orders/Order.js
+++ src/Orders/Order.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Badge, SimpleGrid, Flex, Button } from '@chakra-ui/core';
+import { Box, SimpleGrid, Flex, Button, CircularProgress, CircularProgressLabel } from '@chakra-ui/core';
 
 import QRCode from 'qrcode.react';
 
@@ -18,7 +18,7 @@ export default class Order extends React.Component {
         const { order } = this.props;
         const totalPrice = order.order_items.map(orderItem => parseInt(orderItem.product.price, 10) * orderItem.quantity).reduce(sum, 0);
         const humanStatus = {
-          new: 'В ожидании',
+          new: 'В очереди',
           in_progress: 'Готовится',
           ready: 'Готово!',
         };
@@ -26,29 +26,38 @@ export default class Order extends React.Component {
         return(
           <Box>
             <br />
-            <SimpleGrid columns={2} spacing={1}>
-              <Box textAlign='center'>
-                <Flex align='center' justify='center'>
-                  <QRCode value={`${baseURL}/admin/orders/${order.code}/code`} />
+            <SimpleGrid columns={2} spacing={0} p='4'>
+              <Flex align='center' justify='flex-start'>
+                <QRCode value={`${baseURL}/admin/orders/${order.code}/code`} />
+              </Flex>
+              {humanStatus[order.status] && (
+                <Flex align='center' justify='flex-end'>
+                  <CircularProgress value={order.readiness_percentage} size='128px' thickness={0.1}>
+                    <CircularProgressLabel fontSize='12px'>{humanStatus[order.status]}</CircularProgressLabel>
+                  </CircularProgress>
                 </Flex>
-                {humanStatus[order.status] && <br />}
-                {humanStatus[order.status] && <Box p='4'><Badge w='100%'>{humanStatus[order.status]}</Badge></Box>}
-              </Box>
-              <Box>
-                {order.order_items.map(orderItem => `${orderItem.product.name} (${orderItem.quantity}) `).join(', ')}
-                <Box p='4' textAlign='right'>
-                  {`Всего ${totalPrice} ₴`}
-                </Box>
-              </Box>
+              )}
 
             </SimpleGrid>
+
+            <Box p='4'>
+              {order.order_items.map(orderItem => `${orderItem.product.name} (${orderItem.quantity}) `).join(', ')}
+            </Box>
+            <Box p='4' textAlign='right'>
+              {' '}
+              Всего
+              <b>{totalPrice}</b>
+              {' '}
+              ₴
+            </Box>
+
             
             {!!order.comment.length && <Box m='4' p='4' borderRadius='lg' border='#eee solid 1px'>{order.comment}</Box>}
             {humanStatus[order.status] && (
-            <Box pr='2' textAlign='right'>
+            <Box pr='4' textAlign='right'>
               <Button variant="outline" variantColor='red' size='sm' onClick={this.onCancelOrder}>Отменить</Button>
             </Box>
-)}
+          )}
             <br />
             <hr />
 
